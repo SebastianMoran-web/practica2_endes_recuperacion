a2f49372c8714ba756d32b334c282bce68161f2a
MORO\moran_dcmaguf <moranquispe.sebastianandre@jgvaldemora.org>
2026-03-24 19:23:47 +0100

    Practica

--- practica2_endes_recu/src/main/java/edu/moranQuispeSebastian/Main.java
+++ practica2_endes_recu/src/main/java/edu/moranQuispeSebastian/Main.java
@@ -1,17 +1,37 @@
 package edu.moranQuispeSebastian;
+import entities.alumno;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= ; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        alumno a = new alumno();
+        a.setNombre("Sebas");
+        a.setApellido("Moran");
+        a.setEdad(18);
+        a.setGrado("Drado Superior de DAW");
+        a.setNacionalidad("Española");
+        a.setEmail("moransebas72@gmail.com");
+        a.setPeso(102.00);
+        a.setAltura(1.83);
+
+    System.out.println("Nombre:" + a.getNombre());
+    System.out.println("Apellido:" + a.getApellido());
+    System.out.println("Edad:" + a.getEdad() + " Años");
+    System.out.println("Grado:" + a.getGrado());
+    System.out.println("Nacionalidad:" + a.getNacionalidad());
+    System.out.println("Email:" + a.getEmail());
+    System.out.println("Peso:" + a.getPeso() + " Kilogramos");
+    System.out.println("Altura:" + a.getAltura() + " Metros");
+
+    // Metodo para saber si el alumno es menor o mayor e edad
+
+        boolean mayorDeEdad = alumno.mayorDeEdad(a.getEdad());
+
+        double imc = alumno.imc(a.getPeso(), a.getAltura());
+        System.out.println("IMC:" + imc);
+
     }
+
+
+
 }
---
+++ practica2_endes_recu/src/main/java/entities/alumno.java
@@ -0,0 +1,122 @@
+package entities;
+
+public class alumno {
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String grado;
+    private String nacionalidad;
+    private String email;
+    private double peso;
+    private double altura;
+
+    // constructor sin atributos
+
+    public alumno() {
+
+        this.nombre = "Sin nombre";
+        this.apellido = "Sin apellido";
+        this.edad = 0;
+        this.grado = "No especificado";
+        this.nacionalidad = "No especificada";
+        this.email = "Sin mail";
+        this.peso = 0.0;
+        this.altura = 0.0;
+    }
+
+    //constructor con los atributos
+
+    public alumno(String nombre, String apellido, int edad, String grado, String nacionalidad, String email, double peso, double altura) {
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.grado = grado;
+        this.nacionalidad = nacionalidad;
+        this.email = email;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    //getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGrado() {
+        return grado;
+    }
+    public void setGrado(String grado) {
+        this.grado = grado;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+
+    //metodos static que son saber si es mayor de edad y el IMC del alumno
+
+    public static boolean mayorDeEdad(int edad){
+        if (edad >= 18) {
+            System.out.println("El alumno es mayor de edad");
+            return true;
+        }else{
+            System.out.println("El alumno es mayor de edad");
+            return false;
+        }
+    }
+
+    public static double imc(double peso, double altura){
+
+        if ( altura <= 0) return 0;
+        return peso / (altura * altura);
+    }
+
+
+}
